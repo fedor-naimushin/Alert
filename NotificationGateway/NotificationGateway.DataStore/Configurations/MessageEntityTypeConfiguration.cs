d491c7f6539c874e35348809f744eb25c946c0d1
fedor-naimushin <fedor.naimushin@urfu.me>
2025-01-03 09:46:20 +0500

    Добавил вторую сущность

---
+++ NotificationGateway/NotificationGateway.DataStore/Configurations/MessageEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NotificationGateway.Core;
+
+namespace NotificationGateway.DataStore.Configurations;
+
+public class MessageEntityTypeConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(n => n.Id);
+    }
+}
---
+++ NotificationGateway/NotificationGateway.DataStore/Repositories/Infrastructure/IMessageRepository.cs
@@ -0,0 +1,7 @@
+﻿using NotificationGateway.Core;
+
+namespace NotificationGateway.DataStore.Repositories.Infrastructure;
+
+public interface IMessageRepository : IRepository<Message>
+{
+}
